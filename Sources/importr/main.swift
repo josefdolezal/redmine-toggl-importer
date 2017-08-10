6b8b28c4d66cd3dc6ea3f198207ebda496a4100d
Josef Dolezal <pepik.ml@gmail.com>
2017-08-10 22:24:44 +0200

    Add basic cli support

--- Sources/importr/main.swift
+++ Sources/importr/main.swift
@@ -1,3 +1,19 @@
-import ImporterKit
+//
+//  main.swift
+//  Importr
+//
+//  Created by Josef Dolezal on 08/08/2017.
+//
+//
 
-importTimeEntries()
+import Commander
+
+let main = command(
+    Argument<String>("Redmine Token", description: "The user's personal read/write access token for Redmine API."),
+    Argument<String>("Toggl Token", description: "TThe user's personal read/write access token for Toggl API.")
+) { redmineToken, togglToken in
+    print("Your Redmine Token: \(redmineToken)")
+    print("Your Toggl Token: \(togglToken)")
+}
+
+main.run()
