aebf793782b3cf36672ef50fbc4c380685eb0ad3
Josef Dolezal <pepik.ml@gmail.com>
2017-08-13 21:42:30 +0200

    Create TogglTimeEntry model object

---
+++ Sources/ImporterKit/Model/TogglTimeEntry.swift
@@ -0,0 +1,18 @@
+//
+//  TogglTimeEntry.swift
+//  Importr
+//
+//  Created by Josef Dolezal on 13/08/2017.
+//
+//
+
+struct TogglTimeEntry {
+    let ID: Int
+    let description: String?
+    let start: Date
+    let end: Date
+    let duration: Int
+    let client: String
+    let project: String
+    let tags: [String]
+}
