789954371c59826dca4828ef1eaf730a036273d5
Josef Dolezal <pepik.ml@gmail.com>
2017-08-19 14:28:19 +0200

    WIP: Move project to SPMV4 specs

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "Importr",
+    dependencies: [
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", .upToNextMinor(from: "2.0.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift/", .branch("swift4.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .exact(Version(9, 0, 0, prereleaseIdentifiers: ["alpha", "1"]))),
+        .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from: "0.6.0"))
+    ],
     targets: [
-        Target(
+        .target(
             name: "importr",
             dependencies: [
-                "ImporterKit"
+                .product(name: "Commander"),
+                .target(name: "ImporterKit")
             ]
         ),
-        Target(
-            name: "ImporterKit"
+        .target(
+            name: "ImporterKit",
+            dependencies: [
+                .product(name: "Moya")
+            ]
         )
-    ],
-    dependencies: [
-        .Package(url: "https://github.com/Moya/Moya.git", majorVersion: 8, minor: 0),
-        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6)
     ]
 )
