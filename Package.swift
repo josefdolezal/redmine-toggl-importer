4600967ab6512992bc6f9fea8aaf0ab440b9a5a0
Josef Dolezal <pepik.ml@gmail.com>
2017-08-21 23:31:02 +0200

    Add JSON mapping library

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/Moya/Moya.git", majorVersion: 8, minor: 0),
-        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/josefdolezal/Moya-ModelMapper.git", majorVersion: 4, minor: 1)
     ]
 )
