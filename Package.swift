a288799befa9964617a04aa9d0ee12575f35ab3e
Josef Dolezal <pepik.ml@gmail.com>
2017-09-10 11:38:38 +0200

    Update Moya dependency to 9.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/Moya/Moya.git", majorVersion: 8, minor: 0),
         .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/josefdolezal/Moya-ModelMapper.git", majorVersion: 4, minor: 1)
+        .Package(url: "https://github.com/sunshinejr/Moya-ModelMapper.git", majorVersion: 5, minor: 0)
     ]
 )
